cce609660c05de36be811d964b31ee38ac04146f
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-11-13 12:03:35 -0800

    Bump version: 1.0.3 → 1.0.4

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   module         = "infrahouse/lambda-monitored/aws"
-  module_version = "1.0.3"
+  module_version = "1.0.4"
 
   default_module_tags = {
     created_by_module = local.module
